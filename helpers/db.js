f65266638b5f2e9e9a122a1f38c40cada7786c7f
Sanderson <virgolinos@gmail.com>
2023-07-19 08:15:45 -0300

    wip

--- helpers/db.js
+++ helpers/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://malocagames:A91AgTwjGMDKs1XMCIxLZobsK9NZ99UF@dpg-cin2eu18g3nafl2mn3bg-a.oregon-postgres.render.com/potentesdb', {dialect: 'postgres'});
+const sequelize = new Sequelize('', {dialect: 'postgres'});
 
 module.exports = sequelize;
