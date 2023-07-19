385723c8108a4bc31fa03533334ebe01f746d67e
Sanderson <virgolinos@gmail.com>
2023-07-19 08:19:35 -0300

    wip

--- helpers/db.js
+++ helpers/db.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('', {dialect: 'postgres'});
+require('dotenv').config()
+const {DB_CONNSTRING} = process.env
+const sequelize = new Sequelize(DB_CONNSTRING, {dialect: 'postgres'});
 
 module.exports = sequelize;
